9d0c6b1b05a1d3a4016d5da14ce85ebcdd4bf291
katameqqe <protsyukvitalik@gmail.com>
2026-05-26 18:06:06 -0400

    adding new services for backend

--- src-tauri/nix/configuration.nix
+++ src-tauri/nix/configuration.nix
@@ -3,7 +3,7 @@
   
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true; # Works better for VMs and some hardware
+  boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.device = "nodev";
   boot.initrd.availableKernelModules = [ "virtio_pci" "virtio_blk" "virtio_scsi" "virtio_net" "virtio_console" "xhci_pci" "ahci" "usbhid" "sr_mod" "ata_piix" "uhci_hcd" "sd_mod" ];
 
@@ -12,14 +12,15 @@
     "{{ ssh_key }}"
   ];
 
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 8443 53 ];
+  networking.firewall.allowedUDPPorts = [ 53 51820 ];
 
   services.nginx.enable = true;
 
+  {{ acme_config }}
+
   {{ nextcloud_block }}
   {{ nextcloud_nginx_vhost }}
-
-  {{ acme_config }}
 
   {{ jellyfin_block }}
   {{ jellyfin_nginx_vhost }}
@@ -33,8 +34,24 @@
       ROCKET_ADDRESS = "127.0.0.1";
     };
   };
-
   {{ vaultwarden_nginx_vhost }}
+
+  {{ immich_block }}
+  {{ immich_nginx_vhost }}
+
+  {{ gitea_block }}
+  {{ gitea_nginx_vhost }}
+
+  {{ uptime_kuma_block }}
+  {{ uptime_kuma_nginx_vhost }}
+
+  {{ vikunja_block }}
+  {{ vikunja_nginx_vhost }}
+
+  {{ tailscale_block }}
+
+  {{ adguardhome_block }}
+  {{ adguardhome_nginx_vhost }}
 
   system.stateVersion = "24.05";
 }
